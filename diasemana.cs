e66dbe39fd66649f61e7547b30dbce66ad9a8dee
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-11-19 16:18:13 +0000

    Add files via upload

---
+++ diasemana.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diadasemana
+{
+    class diasemana
+    {
+        private DateTime data = new DateTime().Date;
+
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+        public string envia
+        {
+            get
+            {
+                return "O dia da semana em que você nasceu foi " + data.ToString("dddddd");
+            }
+        }
+    }
+
+}
